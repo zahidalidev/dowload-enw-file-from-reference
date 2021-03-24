b8dbe8bc2eca7ba432a3f845f4533e7efde4121d
zahidalidev <m.zahidalidev@gmail.com>
2021-03-24 12:43:58 +0500

    working with chrom driver

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Chrome()
+# browser = webdriver.Chrome()
+browser = webdriver.Chrome('chromedriver')
 browser.get("https://github.com")
 
-sign_link = browser.find_element_by_link_text(`"Sign in")
+sign_link = browser.find_element_by_link_text("Sign in")
 sign_link.click()
