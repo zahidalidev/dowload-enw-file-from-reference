f020e31dbd5167a20d8d34ede6348babe7419175
zahidalidev <m.zahidalidev@gmail.com>
2021-03-24 12:33:04 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Chrome()
+browser.get("https://github.com")
+
+sign_link = browser.find_element_by_link_text(`"Sign in")
+sign_link.click()
